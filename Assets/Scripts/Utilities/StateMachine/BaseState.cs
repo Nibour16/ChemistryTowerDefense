bf7560a46b5adcc183b41ca0c2bbd021e76ff88a
Nibour16 <ruobinh3@hotmail.com>
2026-01-30 23:57:30 -0700

    Base state machine and state creator (editor utility)

---
+++ Assets/Scripts/Utilities/StateMachine/BaseState.cs
@@ -0,0 +1,15 @@
+public abstract class BaseState
+{
+    protected BaseStateMachine stateMachine; //Owning state machine
+
+    //Composer
+    protected BaseState(BaseStateMachine stateMachine)
+    {
+        //Cache this state machine owned by the state itself
+        this.stateMachine = stateMachine;
+    }
+
+    public abstract void EnterState();
+    public abstract void UpdateState();
+    public abstract void ExitState();
+}
