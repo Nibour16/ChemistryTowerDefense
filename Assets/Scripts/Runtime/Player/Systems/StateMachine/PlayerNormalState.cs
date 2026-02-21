37d63b48207fb8e811f1ae70fdae584da4bae836
Nibour16 <ruobinh3@hotmail.com>
2026-02-20 22:30:07 -0700

    Create Player State Machine

---
+++ Assets/Scripts/Runtime/Player/Systems/StateMachine/PlayerNormalState.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerNormalState : BaseState
+{
+    private PlayerStateMachine _stateMachine;
+
+    #region Initialization
+    public PlayerNormalState(BaseStateMachine  stateMachine) : base(stateMachine) 
+    {
+        ResolveDependencies();
+    }
+
+    private void ResolveDependencies()
+    {
+        _stateMachine = GetStateMachine<PlayerStateMachine>();
+        if (_stateMachine == null)
+        {
+            Debug.LogError($"{nameof(PlayerNormalState)} requires {nameof(PlayerStateMachine)}");
+        }
+    }
+    #endregion
+
+    #region State Lifecycle
+    public override void EnterState() 
+    {
+        // TODO: write logic when entering this state here
+    }
+    public override void UpdateState() 
+    {
+        // TODO: write logic during this state update here
+    }
+    public override void ExitState() 
+    {
+        // TODO: write logic when exiting this state here
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Runtime/Player/Systems/StateMachine/PlayerStateMachine.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerStateMachine : BaseStateMachine
+{
+    #region Keep track of all states
+    // TODO: Create the desired states here, ensure they are all non-serialized and private
+    #endregion
+
+    #region Referencing all of states
+    // TODO: Create the desired state properties here
+    #endregion
+
+    #region Keep track of all supporting components
+    //  TODO: Create supporting components here, ensure they are all private
+    #endregion
+
+    #region Referencing all supporting components
+    //  TODO: Create supporting components here
+    #endregion
+
+    private void Awake()
+    {
+        // TODO: Initialize states and components here
+        // Example: _someState = new SomeState(this);
+    }
+
+    private void Start()
+    {
+        // TODO: Set initial state
+        // Format will be like this: SetState(new SomeState(this));
+    }
+}
