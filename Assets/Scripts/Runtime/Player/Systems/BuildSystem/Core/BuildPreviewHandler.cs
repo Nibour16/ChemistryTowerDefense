74edbc4718059d2a66323b8d2f958c11d7530725
Nibour16 <ruobinh3@hotmail.com>
2026-02-22 05:16:26 -0700

    Update BuildPreviewHandler.cs
    Patch

--- Assets/Scripts/Runtime/Player/Systems/BuildSystem/Core/BuildPreviewHandler.cs
+++ Assets/Scripts/Runtime/Player/Systems/BuildSystem/Core/BuildPreviewHandler.cs
@@ -4,18 +4,50 @@ public class BuildPreviewHandler : BaseBuildSecretary
 {
     private GameObject _ghost;
 
-    public void StartPreview()
+    public void CreateGhost()
     {
-        
+        if (manager.CurrentDefinition == null)
+            return;
+
+        var prefab = manager.CurrentDefinition.TowerPrefab;
+
+        _ghost = Instantiate(prefab);
+
+        MakeGhostVisual(prefab);
     }
 
-    public void UpdatePreview()
+    private void MakeGhostVisual(GameObject ghost)
     {
-        
+        // Stop all momobehaviours
+        foreach (var comp in ghost.GetComponentsInChildren<MonoBehaviour>())
+        {
+            comp.enabled = false;
+        }
+
+        // Stop all collisions
+        foreach (var col in ghost.GetComponentsInChildren<Collider>())
+        {
+            col.enabled = false;
+        }
+
+        // Change the material colour
+        foreach (var renderer in ghost.GetComponentsInChildren<Renderer>())
+        {
+            renderer.material.color = new Color(0, 1, 0, 0.5f);
+        }
     }
 
-    public void StopPreview()
+    public void DestroyGhost()
     {
-        
+        if (_ghost != null)
+            Destroy(_ghost);
+    }
+
+    public void UpdateGhostPosition(Vector3 worldPos)
+    {
+        if (_ghost == null)
+            return;
+
+        _ghost.transform.position = worldPos;
     }
 }
