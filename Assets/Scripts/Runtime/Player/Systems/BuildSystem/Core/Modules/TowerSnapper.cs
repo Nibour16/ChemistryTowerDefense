c6e711c2ec80b1b50cfc7352fd46c75acf5a2e09
Nibour16 <ruobinh3@hotmail.com>
2026-02-23 14:57:52 -0700

    Change Tower Snapper as a build system module

---
+++ Assets/Scripts/Runtime/Player/Systems/BuildSystem/Core/Modules/TowerSnapper.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TowerSnapper
+{
+    private readonly IGridService _grid;
+
+    public TowerSnapper(IGridService grid)
+    {
+        _grid = grid;
+    }
+
+    public bool TryGetSnappedPosition(
+        Vector3 worldPos, out Vector3 snappedPos, out int cellX, out int cellZ)
+    {
+        if (!_grid.WorldToCell(worldPos, out cellX, out cellZ))
+        {
+            snappedPos = default;
+            return false;
+        }
+
+        snappedPos = _grid.GetCellCenter(cellX, cellZ);
+        return true;
+    }
+}
