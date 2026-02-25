741553865dfee8d2a4517e886a8eadf8ef143d59
Nibour16 <ruobinh3@hotmail.com>
2026-02-25 14:38:21 -0700

    Update BaseTower.cs
    Tower rotation

--- Assets/Scripts/Runtime/Tower/Core/BaseTower.cs
+++ Assets/Scripts/Runtime/Tower/Core/BaseTower.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 [RequireComponent(typeof(TowerStat))]
 public abstract class BaseTower : MonoBehaviour
@@ -6,11 +7,15 @@ public abstract class BaseTower : MonoBehaviour
     [Header("General Setting")]
     [SerializeField] private bool drawGizmos = false;
     [SerializeField] private Color debugColour = Color.red;
+    [SerializeField] private float rotationSpeed = 2.0f;
+    [SerializeField] private bool isGroundTower = true;
 
     [Header("Advanced Setting")]
+    [SerializeField] private float aimingTolerance = 5f;
     [SerializeField] private bool checkModuleValidation = true;
 
     private EnemyManager _enemyManager;
+    private EnemyCharacter _currentTarget;
 
     protected TowerStat stat;
     public TowerStat Stat => stat;
@@ -38,27 +43,6 @@ public abstract class BaseTower : MonoBehaviour
         HandleAttackLogic();
     }
 
-    protected abstract void SetupModules();
-
-    protected virtual void HandleAttackLogic()
-    {
-        coolingTimer -= Time.deltaTime;
-
-        if (coolingTimer > 0f) return;
-
-        var targets = detector?.Detect(_enemyManager, this);
-
-        if (targets == null || targets.Count == 0) return;
-
-        var selectedTarget = selector?.Select(targets);
-
-        if (selectedTarget == null) return;
-
-        attacker?.Attack(selectedTarget);
-
-        coolingTimer = stat.AttackDelay;
-    }
-
     private void CheckModules()
     {
         if (detector == null)
@@ -66,15 +50,72 @@ public abstract class BaseTower : MonoBehaviour
             Debug.LogWarning($"{name}: Detector is not assigned, this tower will unable to find target and attack");
             return;
         }
-            
+
         if (selector == null)
         {
             Debug.LogWarning($"{name}: Selector is not assigned, this tower will unable to attack");
             return;
         }
-            
+
         if (attacker == null)
             Debug.LogWarning($"{name}: Attack Module is not assigned");
+    }
+
+    protected abstract void SetupModules();
+
+    protected virtual void HandleAttackLogic()
+    {
+        // If the logic becomes very complex, use state machine instead
+        
+        coolingTimer -= Time.deltaTime;
+
+        if (_currentTarget == null)
+        {
+            var targets = detector?.Detect(_enemyManager, this);
+            if (targets == null || targets.Count == 0) return;
+
+            _currentTarget = selector?.Select(targets);
+            if (_currentTarget == null) return;
+        }
+
+        RotateToTarget(_currentTarget);
+
+        if (!IsFacingTarget(_currentTarget)) return;
+
+        if (coolingTimer <= 0f)
+        {
+            attacker?.Attack(_currentTarget);
+            coolingTimer = stat.AttackDelay;
+        }
+    }
+
+    protected virtual void RotateToTarget(EnemyCharacter target)
+    {
+        GetDirection(target, out Vector3 dir);
+
+        if (dir != Vector3.zero)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(dir);
+            var rot = rotationSpeed * Time.deltaTime;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rot);
+        }
+    }
+
+    protected virtual bool IsFacingTarget(EnemyCharacter target)
+    {
+        GetDirection(target, out Vector3 dir);
+
+        if (dir == Vector3.zero) return true;
+
+        float angle = Vector3.Angle(transform.forward, dir);
+        return angle < aimingTolerance;
+    }
+
+    private void GetDirection(EnemyCharacter target, out Vector3 dir)
+    {
+        dir = target.transform.position - transform.position;
+        if (isGroundTower) dir.y = 0f; // if it is ground tower
     }
 
     #if UNITY_EDITOR
